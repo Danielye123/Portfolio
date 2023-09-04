a8087680574e885bd53d3db95f3d41d460aadaf6
Daniel <daniel_ye.nz@hotmail.com>
2023-09-03 22:38:43 -0700

    project setup

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav.tsx";
+import Hero from "./Hero.tsx";
+import Footer from "./Footer.tsx";
+import Skills from "./Skills.tsx";
+import Services from "./Services.tsx";
+import FeaturedProjects from "./FeaturedProjects.tsx";
+import Recommendations from "./Recommendations.tsx";
+
+export {
+    Nav,
+    Hero,
+    Footer,
+    Skills,
+    Services,
+    FeaturedProjects,
+    Recommendations,
+};
