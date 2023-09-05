9a67fe0f4b000af069bb53d3014f6fc15fba926b
Daniel <daniel_ye.nz@hotmail.com>
2023-09-04 23:53:21 -0700

    setup hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link";
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="flex flex-row min-h-screen bg-[#F6F6F6]">
+
+      <div className="text-black">
+        <div>
+          <h2>HI, I AM DANIEL YE</h2>
+        </div>
+        <div>
+          Professional 
+          <span>Web Developer </span>
+          based in USA
+        </div>
+        <div>
+        Varius amet, integer tellus non eget viverra. Ultrices tellus donec gravida id sed senectus dolor cursus sed. Ullamcorper tellus ac cras nec, pretium laoreet duis. 
+        </div>
+        <Link href="/contact">
+          Contact me
+        </Link>
+
+        <div>
+          Social Media
+        </div>
+      </div>
+
+      <div className="text-black">
+        Image
+      </div>
+    </div>
   )
 }
 
-export default Hero
+export default Hero;
