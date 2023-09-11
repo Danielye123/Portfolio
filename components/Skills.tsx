27cf2a4f5d01b9b66c8d778e5b4e097e20b07960
Daniel <daniel_ye.nz@hotmail.com>
2023-09-10 20:50:03 -0700

    add icons and circles to skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,13 +1,26 @@
 import { JavaScript, css, express, git, github, html, mongodb, motion, next, nodejs, react, tailwind, TypeScript  } from "@/assets"
 import { skills } from "@/constants"
+import Image from "next/image"
 
 const Skills = () => {
   return (
     <section className="flex flex-col justify-start items-center py-[72px] bg-primary-light dark:bg-primary-darkDefault w-full h-[513px]">
-      <h1 className="text-primary-darkDefault font-poppins font-bold text-[48px] leading-[55px] tracking-[-0.01em] underline underline-offset-1 decoration-[15px] decoration-[#FFBE62]">
+      <h1 className="text-primary-darkDefault dark:text-primary-light font-poppins font-bold text-[48px] leading-[55px] tracking-[-0.01em] underline underline-offset-1 decoration-[15px] decoration-[#FFBE62]">
       My skills
       </h1>
-
+      {/* <div className="bg-[#F3F8FF] width-[100px] height-[100px] rounded-full">
+        {skills.imgURL.map(())}
+      </div> */}
+      <div className="flex flex-wrap justify-center pt-[48px] gap-[41px] px-[85px]">
+        {skills.map((skill, index) => (
+          <div 
+            key={index} 
+            className={`m-4 bg-[#F3F8FF] dark:bg-[#192333] w-[100px] h-[100px] rounded-full flex items-center justify-center`}
+          >
+            <Image src={skill.imgURL} alt={`Skill ${index}`} />
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
