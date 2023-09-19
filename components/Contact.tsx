b07f260d5ead24502a081dcffcce254b6cc4ee8e
Daniel <daniel_ye.nz@hotmail.com>
2023-09-19 03:46:35 -0700

    start contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className="w-full h-[474px] bg-[#FFFFFF] flex items-center justify-center">
+            <div className="bg-[#FFBE62] w-[1270px] h-[330px] flex items-center justify-center rounded-3xl">
+                <div className="flex flex-1 items-center justify-center">
+                    Have a project in mind that requires technical expertise?
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    button
+                </div>
+            </div>
+        </div>
+
     )
 }
 
