3b30f5cb019e3cdb098de20b1b17089f5b6a820f
Daniel <daniel_ye.nz@hotmail.com>
2023-09-27 00:51:58 -0700

    begin with backgorund of contact component

---
+++ assets/contact/index.js
@@ -0,0 +1,7 @@
+import ellipse from './Ellipse2.svg';
+import rectangle from './Rectangle1.svg';
+
+export {
+    ellipse,
+    rectangle,
+}
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import {ellipse, rectangle} from "../assets/contact/index";
 
 const Contact = () => {
     return (
-<div className="w-full px-[85px] h-[474px] bg-[#FFFFFF] flex items-center justify-center">
-    <div className="bg-[#FFBE62] w-full px-[85px] h-[330px] flex items-center justify-between rounded-3xl">
-        <div className="font-poppins text-[48px] font-bold leading-[55px] tracking-[-0.01em] text-[#151E2C] w-[520px] h-[165px] ml-[75px]">
-            Have a project in mind that requires technical expertise?
+        <div className="w-full px-[85px] h-[474px] bg-[#FFFFFF] relative flex items-center justify-center">
+            
+            <div className="bg-[#FFBE62] w-full px-[85px] h-[330px] flex items-center justify-between rounded-3xl z-0">
+            <div className="absolute top-[98.88px] left-[897.96px] transform rotate-90 z-10">
+                <Image src={ellipse} alt="Pattern Ellipse" width={97.35} height={180.86} />
+            </div>
+
+            <div className="absolute top-[0.44px] left-[1247.11px] z-10">
+                <Image src={rectangle} alt="Pattern Rectangle" width={181.19} height={168.29} />
+            </div>
+            
+                <div className="font-poppins text-[48px] font-bold leading-[55px] tracking-[-0.01em] text-[#151E2C] w-[520px] h-[165px] ml-[75px]">
+                    Have a project in mind that requires technical expertise?
+                </div>
+                <button className="w-[323px] h-auto py-[20px] px-[12px] rounded-full bg-[#0252CD] text-white">
+                    Get in touch with me
+                </button>
+            </div>
         </div>
-        <button className="w-[323px] h-auto py-[20px] px-[12px] rounded-full bg-[#0252CD] text-white">
-            Get in touch with me
-        </button>
-    </div>
-</div>
-
-
-
     )
 }
-{/* <div className="w-full h-[474px] bg-[#FFFFFF] flex items-center justify-center">
-    <div className="bg-[#FFBE62] w-[1270px] h-[330px] flex items-center justify-between px-[75px] rounded-3xl">
-        <div className="font-poppins text-[48px] font-bold leading-[55px] tracking-[-0.01em] text-[#151E2C]">
-            Have a project in mind that requires technical expertise?
-        </div>
-        <button className="w-[323px] h-auto py-[20px] px-[12px] rounded-full bg-[#0252CD] text-white">
-            Get in touch with me
-        </button>
-    </div>
-</div> */}
 
-export default Contact
+export default Contact;
