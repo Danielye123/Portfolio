fb236513b855d2e39719077abae92abd92427b2a
Daniel <daniel_ye.nz@hotmail.com>
2023-10-04 04:31:59 -0700

    add margins to even cards

--- components/Services.tsx
+++ components/Services.tsx
@@ -20,7 +20,9 @@ const Services = () => {
           {services.map((service, index) => (
             <div
               key={index}
-              className="w-[285px] h-[265px] border rounded-lg shadow-md dark:border-[#1F2E48] dark:shadow-md p-4 flex flex-col items-start bg-[#F3F8FF] dark:bg-[#192333] hover:bg-[#0252CD] dark:hover:bg-[#428DFF] hover:text-primary-light service-card transition-transform transform hover:scale-105"
+              className={`w-[285px] h-[265px] border rounded-lg shadow-md dark:border-[#1F2E48] dark:shadow-md p-4 flex flex-col items-start bg-[#F3F8FF] dark:bg-[#192333] hover:bg-[#0252CD] dark:hover:bg-[#428DFF] hover:text-primary-light service-card transition-transform transform hover:scale-105 ${
+                index % 2 === 0 ? "" : "mt-[20px]" // Apply margin to even cards
+              }`}
             >
               <Image
                 src={service.imgUrl}
