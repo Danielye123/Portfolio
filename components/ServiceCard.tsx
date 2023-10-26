3732b9cc758f747a4b576753c11f9d26192c9aa9
Daniel <daniel_ye.nz@hotmail.com>
2023-10-26 00:10:17 -0700

    services cards box shadow fix

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -2,15 +2,15 @@ import { services } from "@/constants";
 
 const ServiceCard = () => {
   return (
-    <div className="flex flex-row flex-wrap gap-4 mt-8 md:mt-16 justify-center">
+    <div className="flex flex-row flex-wrap gap-[40px] mt-8 md:mt-16 justify-center">
       {services.map((service, index) => (
         <div
           key={index}
-          className={`group w-[285px] h-[265px] rounded-lg shadow-lg border shadow-[#E0ECFF] dark:shadow-[#1F2E48] dark:border-[#1F2E48] dark:shadow-md p-4 flex flex-col items-start bg-[#F3F8FF] dark:bg-[#192333] text-black service-card transition-transform transform 
-    hover:scale-105 hover:mt-4 hover:bg-[#0252CD] hover:text-[#F3F8FF] hover:shadow-lg hover:shadow-[#0252CD] dark:hover:shadow-lg dark:hover:shadow-[#428DFF] serviceCard`}
+          className={`group w-[285px] h-[265px] rounded-lg shadow-lg border custom-box-shadow-card dark:shadow-[#1F2E48] dark:border-[#1F2E48] dark:shadow-md p-4 flex flex-col items-start bg-[#F3F8FF] dark:bg-[#192333] text-black service-card transition-transform transform 
+    hover:scale-105 hover:mt-4 hover:bg-[#0252CD] hover:text-[#F3F8FF] hover:shadow-lg custom-box-shadow dark:hover:shadow-lg dark:hover:shadow-[#428DFF] serviceCard`}
         >
           <div
-            className={`mt-2 md:mt-0 mb-4 md:mb-8 md:w-[62px] md:h-[62px] bg-[#0252CD] dark:bg-[#428DFF] rounded-lg hoverBgChange flex justify-center items-center`}
+            className={`mt-2 md:mt-0 mb-4 md:mb-8 md:w-[62px] md:h-[62px] bg-[#0252CD] dark:bg-[#428DFF] rounded-lg hoverBgChange flex justify-center items-center shadow-lg custom-box-shadow-blue`}
           >
             <service.Component
               className="text-white group-hover:text-[#0252CD] dark:text-white dark:group-hover:text-[#428DFF] my-auto transform translate-x-7 translate-y-7 md:translate-x-[18px] md:translate-y-5"
