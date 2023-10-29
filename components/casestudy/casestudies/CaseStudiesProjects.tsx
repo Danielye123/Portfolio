745e1d64dbcd79b8dee04a086a4685a3fc751ec9
Daniel <daniel_ye.nz@hotmail.com>
2023-10-28 22:16:27 -0700

    fix whitespace for case studies page in mobile view

--- components/casestudy/casestudies/CaseStudiesProjects.tsx
+++ components/casestudy/casestudies/CaseStudiesProjects.tsx
@@ -9,11 +9,11 @@ import React from "react";
 
 const CaseStudiesProjects = () => {
   return (
-    <div className="flex flex-wrap justify-center items-start min-h-screen py-[72px] bg-[#FFFFFF] dark:bg-[#151E2C] px-[152px] gap-[20px]">
+    <div className="flex flex-wrap justify-center items-start min-h-screen py-[72px] bg-[#FFFFFF] dark:bg-[#151E2C] px-[152px] gap-[30px] md:gap-[36px]">
       {/* taking out px-[152px] makes other screens 2 by 2*/}
       {/* Box 1 */}
       <div className="flex flex-col">
-        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#5CAFFC] flex flex-col justify-end items-center rounded-lg px-[36px] mb-6">
+        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#5CAFFC] flex flex-col justify-end items-center rounded-lg px-[36px] mb-3">
           <Link href="/morrent" className="md:mb-[-10px] mb-0">
             <Image
               src={MorrentCaseStudies}
@@ -23,19 +23,19 @@ const CaseStudiesProjects = () => {
             />
           </Link>
         </div>
-        <div className="text-left pl-[50px] pb-[36px] md:pb-0">
-          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[6px]">
+        <div className="text-left pl-[50px]">
+          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[4px]">
             Morrent
           </p>
-          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7] pb-[20px] md:pb-0">
+          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7]">
             Car Rental Application
           </p>
         </div>
       </div>
 
       {/* Box 2 */}
-      <div className="flex flex-col pb-[36px]">
-        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#FF6934] flex flex-col justify-end items-center rounded-lg mx-6 mb-6">
+      <div className="flex flex-col">
+        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#FF6934] flex flex-col justify-end items-center rounded-lg px-[36px] mb-3">
           <Link href="/hipnode" className="md:mb-[-10px] mb-0">
             <Image
               src={HipNodeCaseStudies}
@@ -47,10 +47,10 @@ const CaseStudiesProjects = () => {
           </Link>
         </div>
         <div className="text-left pl-[50px]">
-          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[6px]">
+          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[4px]">
             Hipnode
           </p>
-          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7] pb-[20px] md:pb-0">
+          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7]">
             Social Platform for Developers
           </p>
         </div>
@@ -58,7 +58,7 @@ const CaseStudiesProjects = () => {
 
       {/* Box 3 */}
       <div className="flex flex-col">
-        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#DB202C] flex flex-col justify-end items-center rounded-lg px-[36px] mb-6">
+        <div className="md:w-[550px] md:h-[328px] w-[345px] h-[205px] bg-[#DB202C] flex flex-col justify-end items-center rounded-lg px-[36px] mb-3">
           <Link href="/filmpire" className="md:mb-[-10px] mb-0">
             <Image
               src={FilmpireCaseStudies}
@@ -69,10 +69,10 @@ const CaseStudiesProjects = () => {
           </Link>
         </div>
         <div className="text-left pl-[50px]">
-          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[6px]">
+          <p className="font-poppins font-semibold md:text-[32px] text-[20px] md:leading-[34px] leading-[26px] text-[#151E2C] dark:text-[#FFFFFF] md:pb-[10px] pb-[4px]">
             Filmpire
           </p>
-          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7] pb-[20px] md:pb-0">
+          <p className="font-poppins font-normal md:text-[20px] text-[14px] md:leading-[30px] leading-[21px] text-[#6F74A7]">
             AI-Powered Movie Application
           </p>
         </div>
