a78e080abea37b77999c00c1f810f78bbe9c7f19
Daniel <daniel_ye.nz@hotmail.com>
2023-11-02 23:32:27 -0700

    adjust responsive design

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,14 +3,17 @@ import Image from "next/image";
 
 const Skills = () => {
   return (
-    <section className="flex flex-col justify-start items-center md:py-[72px] md:px-[85px] sm:py-[46px] sm:px-[24px] bg-primary-light dark:bg-primary-darkDefault">
+    <section className="flex flex-col justify-start items-center md:py-[72px] md:px-[85px] py-[40px] bg-primary-light dark:bg-primary-darkDefault">
 <h1 className="text-primary-darkDefault dark:text-primary-light font-poppins font-bold text-[36px] md:text-[48px] leading-[41px] md:leading-[55px] tracking-[-0.01em] sm:my-0 md:my-0 max-w-[390px] my-10">
         <div className="inline-block relative">
           <span className="relative z-10">My Skills</span>
           <div className="absolute left-0 bottom-0 h-[15px] w-full bg-[#ffbe62] z-0" />
         </div>
       </h1>
-      <div className="flex flex-row flex-wrap justify-center py-10 px-[24px] md:px-[150px] gap-8">
+      {/*className="grid grid-cols-4 gap-x-[2.5rem] gap-y-12 md:flex md:flex-wrap md:justify-center lg:gap-[4rem] 
+          xl:gap-x-[2.8916rem] xl:gap-y-[4.13rem]" */}
+      <div className="grid grid-cols-4 gap-x-[2.5rem] gap-y-8 md:flex md:flex-wrap md:justify-center lg:gap-[4rem] 
+          xl:gap-x-[2.8916rem] xl:gap-y-[4.13rem] py-10 px-6 md:px-[150px]">
         {skills.map((skill, index) => (
           <div
             key={index}
