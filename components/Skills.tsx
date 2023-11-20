3e68115e583294fdda7c025b8a9fc5cdab38156a
Daniel <daniel_ye.nz@hotmail.com>
2023-11-19 21:58:24 -0800

    fix skills section image and circle sizing on mobile screens and padding on md screens

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Skills = () => {
   return (
-    <section className="flex flex-col justify-start items-center md:py-[72px] md:px-[85px] py-[40px] bg-primary-light dark:bg-primary-darkDefault w-full">
+    <section className="flex flex-col justify-start items-center md:py-[72px] lg:px-[85px] py-[40px] bg-primary-light dark:bg-primary-darkDefault w-full">
 <h1 className="text-primary-darkDefault dark:text-primary-light font-poppins font-bold text-[36px] md:text-[48px] leading-[41px] md:leading-[55px] tracking-[-0.01em] pb-[40px] max-w-[390px] ">
         <div className="inline-block relative">
           <span className="relative z-10">My Skills</span>
@@ -18,11 +18,11 @@ const Skills = () => {
         {skills.map((skill, index) => (
           <div
             key={index}
-            className={`m-4 bg-[#F3F8FF] hover:bg-[#FFFFFF] hover:drop-shadow-2xl dark:hover:drop-shadow-2xl transform transition-transform hover:scale-110 dark:bg-[#192333] w-[54px] h-[52px] md:w-[100px] md:h-[100px] rounded-full flex items-center justify-center
-            grayscale hover:grayscale-0 ease-in
+            className={`m-4 bg-[#F3F8FF] hover:bg-[#FFFFFF] hover:drop-shadow-2xl dark:hover:drop-shadow-2xl transform transition-transform hover:scale-110 dark:bg-[#192333] w-[52px] h-[52px] sm:w-[100px] sm:h-[100px] rounded-full flex items-center justify-center
+            grayscale hover:grayscale-0 ease-in opacity-80 hover:opacity-100
             ${index === 0 && 'md:order-1'}`}
           >
-            <Image src={skill.imgURL} alt={`Skill ${index}`} />
+            <Image src={skill.imgURL} alt={`Skill ${index}`} className="sm:w-[50px] sm:h-[50px] w-[26px] h-[26px]" />
           </div>
         ))}
       </div>
