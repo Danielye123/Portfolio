60b66e9235b61b14eb2a9125055c7a92d78d5de0
Daniel <daniel_ye.nz@hotmail.com>
2023-11-25 23:04:53 -0800

    add default to light theme

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider } from "next-themes";
 import { useState, useEffect } from "react";
 
-const Theme = ({ children }: any) => {
+const Theme = ({ children }: { children: React.ReactNode }) => {
   const [mounted, setMounted] = useState(false);
 
   //fix hydration error
@@ -14,7 +14,11 @@ const Theme = ({ children }: any) => {
   if (!mounted) {
     return <>{children}</>;
   }
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Theme;
