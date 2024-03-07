cac6d0c2d06e283d5a2894e7ac6e62b4e7c5fb03
Daniel <daniel_ye.nz@hotmail.com>
2024-03-07 02:40:50 -0800

    complete making projects fully reusable/dynamic, delete unnecessary files for the non-dynamic files

--- components/index.js
+++ components/index.js
@@ -17,36 +17,6 @@ import CaseStudiesHero from "./casestudy/casestudies/CaseStudiesHero.tsx";
 import CaseStudiesProjects from "./casestudy/casestudies/CaseStudiesProjects.tsx";
 import CaseStudiesOther from "./casestudy/casestudies/CaseStudiesOther.tsx";
 
-import MorrentHero from "./casestudy/morrent/MorrentHero.tsx";
-import MorrentRole from "./casestudy/morrent/MorrentRole.tsx";
-import MorrentTech from "./casestudy/morrent/MorrentTech.tsx";
-import MorrentDescription from "./casestudy/morrent/MorrentDescription.tsx";
-import MorrentProblem from "./casestudy/morrent/MorrentProblem.tsx";
-import MorrentFigmaDesign from "./casestudy/morrent/MorrentFigmaDesign.tsx";
-import MorrentProcess from "./casestudy/morrent/MorrentProcess.tsx";
-import MorrentChallenges from "./casestudy/morrent/MorrentChallenges.tsx";
-import MorrentCaseStudiesOther from "./casestudy/morrent/MorrentCaseStudiesOther.tsx";
-
-import FilmpireHero from "./casestudy/filmpire/FilmpireHero.tsx";
-import FilmpireRole from "./casestudy/filmpire/FilmpireRole.tsx";
-import FilmpireTech from "./casestudy/filmpire/FilmpireTech.tsx";
-import FilmpireDescription from "./casestudy/filmpire/FilmpireDescription.tsx";
-import FilmpireProblem from "./casestudy/filmpire/FilmpireProblem.tsx";
-import FilmpireFigmaDesign from "./casestudy/filmpire/FilmpireFigmaDesign.tsx";
-import FilmpireProcess from "./casestudy/filmpire/FilmpireProcess.tsx";
-import FilmpireChallenges from "./casestudy/filmpire/FilmpireChallenges.tsx";
-import FilmpireCaseStudiesOther from "./casestudy/filmpire/FilmpireCaseStudiesOther.tsx";
-
-import HipnodeHero from "./casestudy/hipnode/HipnodeHero.tsx";
-import HipnodeRole from "./casestudy/hipnode/HipnodeRole.tsx";
-import HipnodeTech from "./casestudy/hipnode/HipnodeTech.tsx";
-import HipnodeDescription from "./casestudy/hipnode/HipnodeDescription.tsx";
-import HipnodeProblem from "./casestudy/hipnode/HipnodeProblem.tsx";
-import HipnodeFigmaDesign from "./casestudy/hipnode/HipnodeFigmaDesign.tsx";
-import HipnodeProcess from "./casestudy/hipnode/HipnodeProcess.tsx";
-import HipnodeChallenges from "./casestudy/hipnode/HipnodeChallenges.tsx";
-import HipnodeCaseStudiesOther from "./casestudy/hipnode/HipnodeCaseStudiesOther.tsx";
-
 import Frontend from "./svgComponents/frontend.tsx";
 import Backend from "./svgComponents/backend.tsx";
 import Analytics from "./svgComponents/Analytics.tsx";
@@ -73,37 +43,6 @@ export {
     CaseStudiesProjects,
     CaseStudiesOther,
     HeaderMobileModule,
-
-
-    MorrentHero,
-    MorrentRole,
-    MorrentTech,
-    MorrentDescription,
-    MorrentProblem,
-    MorrentFigmaDesign,
-    MorrentProcess,
-    MorrentChallenges,
-    MorrentCaseStudiesOther,
-
-    FilmpireHero,
-    FilmpireRole,
-    FilmpireTech,
-    FilmpireDescription,
-    FilmpireProblem,
-    FilmpireFigmaDesign,
-    FilmpireProcess,
-    FilmpireChallenges,
-    FilmpireCaseStudiesOther,
-
-    HipnodeHero,
-    HipnodeRole,
-    HipnodeTech,
-    HipnodeDescription,
-    HipnodeProblem,
-    HipnodeFigmaDesign,
-    HipnodeProcess,
-    HipnodeChallenges,
-    HipnodeCaseStudiesOther,
 
     Frontend,
     Backend,
